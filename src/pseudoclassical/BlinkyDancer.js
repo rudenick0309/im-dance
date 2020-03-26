bfdfc4a013853d8405a96a91463de61073a160f6
lmt <1571min@naver.com>
2020-03-26 17:57:55 +0900

    update Dancer

--- src/pseudoclassical/BlinkyDancer.js
+++ src/pseudoclassical/BlinkyDancer.js
@@ -4,10 +4,22 @@ if (typeof window === 'undefined') {
 
 // blinkyDancer를 pseudoclassical한 방식으로 리팩토링하세요
 // 참고로, constructor는 대문자로 이름을 시작하는 것이 관례입니다
-function BlinkyDancer () {
+function BlinkyDancer() {
   // your code here
-
+  Dancer.apply(this, arguments);
 }
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+BlinkyDancer.prototype.consturctor = BlinkyDancer;
+
+BlinkyDancer.prototype.step = () => {
+  Dancer.prototype.step();
+  let style = this.$node.style;
+  style.display = style.display === 'none' ? 'inline-block' : 'none';
+};
+
+//console.log(BlinkyDancer.setPosition(7, 10));
+// let testDancer = new BlinkyDancer();
+// console.log(testDancer.setPosition(7, 10));
 
 // you don't have to worry about this code. this is for testing.
 if (typeof window === 'undefined') {
--- src/pseudoclassical/Dancer.js
+++ src/pseudoclassical/Dancer.js
@@ -1,15 +1,41 @@
 if (typeof window === 'undefined') {
   var jsdom = require('jsdom');
   var { JSDOM } = jsdom;
-  var { document } = (new JSDOM('')).window;
+  var { document } = new JSDOM('').window;
 } // you don't have to worry about this code. this is for testing.
 
 // blinkyDancer를 pseudoclassical한 방식으로 리팩토링하세요
 // 참고로, constructor는 대문자로 이름을 시작하는 것이 관례입니다
-function Dancer () {
-  // your code here
 
+function Dancer(top, left, timeBetweenSteps) {
+  // this.dancer={};
+  const createDancerElement = () => {
+    let elDancer = document.createElement('span');
+    elDancer.className = 'dancer';
+    return elDancer;
+  };
+
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.$node = createDancerElement();
 }
+
+Dancer.prototype.step = () => {
+  setTimeout(this.dancers.step, this.timeBetweenSteps);
+};
+
+Dancer.prototype.setPosition = (top, left) => {
+  // console.log('Dancer의 this : ' + this.$node);
+  Object.assign(this.$node.style, {
+    top: `${top}px`,
+    left: `${left}px`
+  });
+};
+
+//console.log('Dancer: ' + Dancer.prototype.setPosition(7, 10));
+
+// Dancer.prototype.setPosition
 
 // you don't have to worry about this code. this is for testing.
 if (typeof window === 'undefined') {
