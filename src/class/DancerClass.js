7cde0e8ab6098bc996ec5754737159a523ab10cc
lmt <1571min@naver.com>
2020-03-27 14:39:59 +0900

    update es6

--- src/class/DancerClass.js
+++ src/class/DancerClass.js
@@ -1,7 +1,7 @@
 if (typeof window === 'undefined') {
   var jsdom = require('jsdom');
   var { JSDOM } = jsdom;
-  var { document } = (new JSDOM('')).window;
+  var { document } = new JSDOM('').window;
 } // you don't have to worry about this code. this is for testing.
 
 // dancer를 class 키워드를 써서 ES6 방식으로 리팩토링하세요
@@ -9,6 +9,31 @@ if (typeof window === 'undefined') {
 class DancerClass {
   // your code here
 
+  constructor(top, left, timeBetweenSteps) {
+    const createDancerElement = () => {
+      let elDancer = document.createElement('span');
+      elDancer.className = 'dancer';
+      return elDancer;
+    };
+
+    this.top = top;
+    this.left = left;
+    this.timeBetweenSteps = timeBetweenSteps;
+    this.$node = createDancerElement();
+
+    this.step();
+    this.setPostion(top, left);
+  }
+
+  step() {
+    setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  }
+  setPostion(top, left) {
+    Object.assign(this.$node.style, {
+      top: `${top}px`,
+      left: `${left}px`
+    });
+  }
 }
 
 // you don't have to worry about this code. this is for testing.
